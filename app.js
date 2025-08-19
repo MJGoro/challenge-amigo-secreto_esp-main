7b815a65d26216b38155e3341c4326d28348c6e4
Joaquin Gorosito <marianojoaquingorosito@gmail.com>
2025-08-19 20:18:16 -0300

    Se crea array y funciones agregarAmigo/limpiarCaja

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function agregarAmigo(){
+
+    let nombreAmigo = document.getElementById("amigo").value;
+    if(nombreAmigo == ""){
+        alert("Por favor insertar nombre");
+    }
+
+    limpiarCaja();
+    console.log(nombreAmigo);
+}
+    agregarAmigo();
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value = "";
+}
+
