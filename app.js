458a6e8225c8cef4c9d6ae15bb25325a24844539
Joaquin Gorosito <marianojoaquingorosito@gmail.com>
2025-08-21 20:22:54 -0300

    Borro llamado a función que estaba de mas

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 let nombresDeAmigos = [];
 
 function agregarAmigo(){
-
     let nombreAmigo = document.getElementById("amigo").value;
     
     if(nombreAmigo === ""){
-        alert("Por favor insertar nombre");
+        alert("Por favor insertar un nombre");
     } else {
        nombresDeAmigos.push(nombreAmigo);
     } // Verifica si nombre colocado es valido y lo almacena en el array
 
     limpiarCaja(); // Deja vacia la caja para que el usuario coloque otro nombre
-    console.log(nombreAmigo);
-    console.log(nombresDeAmigos);
 }
-    agregarAmigo();
-
+    
 function limpiarCaja(){
     document.querySelector("#amigo").value = "";
 }
